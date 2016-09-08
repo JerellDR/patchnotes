5d2c29b9a2bfa16dc1cb61a678aaa4dc2c9adf7a
Jerell Robinson <jdub03@gmail.com>
2016-09-07 20:58:26 -0400

    tweak control schemes.

--- controlConfigurations.js
+++ controlConfigurations.js
@@ -1,31 +1,32 @@
-function controlConfigurations(){
+function controlConfigurations() {
     this.customControls1 = {
-      Common : {
-        SWITCH : 75,
-        CAMERA : 67
-      },
-      //SHIP
-      Ship : {
-        ACCEL : 83,
-        DECEL : 87,
-        TURNRIGHT : 68,
-        TURNLEFT : 65,
-        SHOOT : 73,
-        SPECIAL: 16
-      },
-      //TURNRIGHT
+        Common: {
+            SWITCH: 75,
+            CAMERA: 67
+        },
+        //SHIP
+         //TODO change identical to regular control scheme
+        Ship: {
+            ACCEL: 83,  // W
+            DECEL: 87, // S
+            TURNRIGHT: 68, // D
+            TURNLEFT: 65, //A
+            SHOOT: 73, // I
+            SPECIAL: 16 // Shift
+        },
+        //TURNRIGHT
 
-      //BOT
-      Bot: {
-        ACCEL : 87,
-        DECEL : 83,
-        TURNLEFT : 65,
-        TURNRIGHT : 68,
-        STRAFELEFT : 74,
-        STRAFERIGHT : 76,
-        SHOOT : 73,
-        SPECIAL : 16
-      }
+        //BOT
+        Bot: {
+            ACCEL: 87,
+            DECEL: 83,
+            TURNLEFT: 65,
+            TURNRIGHT: 68,
+            STRAFELEFT: 74,
+            STRAFERIGHT: 76,
+            SHOOT: 73,
+            SPECIAL: 16
+        }
     };
     this.customControls2 = {
         Common: {
@@ -49,7 +50,7 @@ function controlConfigurations(){
             DECEL: 83,
             TURNLEFT: 65,
             TURNRIGHT: 68,
-            STRAFELEFT: 74, // J
+            STRAFELEFT: 74,
             STRAFERIGHT: 76,
             SHOOT: 73,
             SPECIAL: 16
@@ -60,16 +61,16 @@ function controlConfigurations(){
             SWITCH: 75,
             CAMERA: 67
         },
-        //SHIP
+        //SHIP 
+        //TODO change identical to custom1 and regular control scheme
         Ship: {
-            ACCEL: 39, // Right Arrow
-            DECEL: 32, //Space
-            TURNRIGHT: 68, // D key
-            TURNLEFT: 65, // A key
-            SHOOT: 97,  // numpad 1
-            SPECIAL: 96  // numpad 0
+            ACCEL: 83,  // W
+            DECEL: 87, // S
+            TURNRIGHT: 68, // D
+            TURNLEFT: 65, //A
+            SHOOT: 73, // I
+            SPECIAL: 16 // Shift
         },
-        //TURNRIGHT
 
         //BOT
         Bot: {
@@ -90,12 +91,12 @@ function controlConfigurations(){
         },
         //SHIP
         Ship: {
-            ACCEL: 38, // Arrow up
-            DECEL: 40, //Arrow Down
-            TURNRIGHT: 39, // Arrow Right
-            TURNLEFT: 37, // Arrow Left
-            SHOOT: 70,  // F
-            SPECIAL: 32  // Space
+            ACCEL: 83,  // W
+            DECEL: 87, // S
+            TURNRIGHT: 68, // D
+            TURNLEFT: 65, //A
+            SHOOT: 97,  // numpad1 numlock
+            SPECIAL: 96  // numpad0 numlock
         },
         //TURNRIGHT
 
@@ -105,7 +106,7 @@ function controlConfigurations(){
             DECEL: 83,
             TURNLEFT: 65,
             TURNRIGHT: 68,
-            STRAFELEFT: 74, // J
+            STRAFELEFT: 74,
             STRAFERIGHT: 76,
             SHOOT: 73,
             SPECIAL: 16
@@ -122,8 +123,8 @@ function controlConfigurations(){
             DECEL: 40, //Arrow Down
             TURNRIGHT: 39, // Arrow Right
             TURNLEFT: 37, // Arrow Left
-            SHOOT: 70,  // F
-            SPECIAL: 32  // Space
+            SHOOT: 97,  // numpad1 numlock
+            SPECIAL: 96  // numpad0 numlock
         },
         //TURNRIGHT
 
@@ -133,7 +134,7 @@ function controlConfigurations(){
             DECEL: 83,
             TURNLEFT: 65,
             TURNRIGHT: 68,
-            STRAFELEFT: 74, // J
+            STRAFELEFT: 74,
             STRAFERIGHT: 76,
             SHOOT: 73,
             SPECIAL: 16
@@ -146,11 +147,11 @@ function controlConfigurations(){
         },
         //SHIP
         Ship: {
-            ACCEL: 38, // Arrow up
-            DECEL: 40, //Arrow Down
-            TURNRIGHT: 39, // Arrow Right
-            TURNLEFT: 37, // Arrow Left
-            SHOOT: 70,  // F
+            ACCEL: 87, // W
+            DECEL: 83, //S
+            TURNRIGHT: 68, // D key
+            TURNLEFT: 65, // A key
+            SHOOT: 73, // I
             SPECIAL: 32  // Space
         },
         //TURNRIGHT
@@ -161,7 +162,7 @@ function controlConfigurations(){
             DECEL: 83,
             TURNLEFT: 65,
             TURNRIGHT: 68,
-            STRAFELEFT: 74, // J
+            STRAFELEFT: 74,
             STRAFERIGHT: 76,
             SHOOT: 73,
             SPECIAL: 16
